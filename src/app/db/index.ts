2538a7b05922d795a30680e13094dc18b3f577b2
jveselkov <jveselkov@bk.ru>
2025-01-22 17:39:12 +0500

    add drizzle to project

--- src/app/db/index.ts
+++ src/app/db/index.ts
@@ -1,6 +1,5 @@
-import postgres from 'postgres';
-
 import { drizzle } from 'drizzle-orm/postgres-js';
+import postgres from 'postgres';
 
 if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
 
